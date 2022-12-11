7fd56861f678dbb75eddfb363655031ee667da6e
sushankreddy04 <120310925+sushankreddy04@users.noreply.github.com>
2022-12-11 20:11:33 +0530

    Add files via upload

---
+++ multiplication.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter the value of a:\n");
+    scanf("%d",&a);
+    printf("enter the value of b:\n");
+    scanf("%d",&b);
+    int result=a*b;
+    printf("\n%d",result);
+    return 0;
+}
