c32228bbda128d3f0119eb2819a0eb1a3da371d4
sushankreddy04 <baddipadigesushank@gmail.com>
2022-12-12 21:40:24 +0530

    switch case using vowels.c

---
+++ switch case using vowels.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int main()
+{
+    char ch;
+    printf("please enter any alphabet:");
+    scanf("%d",&ch);
+    switch(ch)
+    {
+        case'a':
+        printf("vowel");
+        break;
+        case'e':
+        printf("vowel");
+        break;
+        case'i':
+        printf("vowel");
+        break;
+        case'o':
+        printf("vowel");
+        break;
+        case'A':
+        printf("vowel");
+        break;
+        case'E':
+        printf("vowel");
+        break;
+        case'I':
+        printf("vowel");
+        break;
+        case'o':
+        printf("vowel");
+        break;
+        case'U':
+        printf("vowel");
+        break;
+        default:
+        printf("consonant");
+
+
+
+
+    }
+    return o
+}
