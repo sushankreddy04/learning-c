aa54410a71630141abcf4ea71f2d6db9e852a03f
sushankreddy04 <baddipadigesushank@gmail.com>
2022-12-13 21:55:44 +0530

    programs using if and else.c

---
+++ programs  using if and else.c
@@ -0,0 +1,20 @@
+/*required age for voting*/
+
+#include<stdio.h>
+int main()
+{
+    int x;
+    printf("enter you age:");
+    scanf("%d",&x);
+    if(x>18)
+{
+    printf("you are aligible for voting:");
+}
+
+{
+ if(x<18)
+    printf("you are not aligible for voting");
+    return 0;
+}
+
+}
