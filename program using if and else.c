09424b01da85dcfa67b686ea47f065c7dba054ed
sushankreddy04 <baddipadigesushank@gmail.com>
2022-12-13 22:01:48 +0530

    program using if and else.c

---
+++ program using if and else.c
@@ -0,0 +1,20 @@
+/*required age for voting*/
+
+#include<stdio.h>
+int main()
+{
+    int x;
+    printf("enter you age:");
+    scanf("%d",&x);
+    if(x>18)
+{
+    printf("you are eligible for voting:");
+}
+
+{
+ if(x<18)
+    printf("you are not eligible for voting");
+    return 0;
+}
+
+}
